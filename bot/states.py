fdc8839f2039c5eda51380b1ebbe891db68c6174
Egor Arhipov <egorarhipov@MacBook-Pro.local>
2025-05-13 23:31:45 +0300

    edit

--- bot/states.py
+++ bot/states.py
@@ -1,15 +1,5 @@
 from aiogram.fsm.state import State, StatesGroup
 
-class PointStates(StatesGroup):
-    waiting_for_name = State()
-    waiting_for_description = State()
-    waiting_for_location = State()
-    waiting_for_content_type = State()
-    waiting_for_text = State()
-    waiting_for_audio = State()
-    waiting_for_photo = State()
-    waiting_for_photo_text = State()
-
 class RouteStates(StatesGroup):
     # Состояния для создания маршрута
     waiting_for_route_name = State()
@@ -22,15 +12,15 @@ class RouteStates(StatesGroup):
     waiting_for_point_name = State()
     waiting_for_point_description = State()
     waiting_for_point_location = State()
-    waiting_for_point_text = State()
-    waiting_for_point_photo = State()
-    waiting_for_point_audio = State()
 
     # Состояния для редактирования точки
     editing_point = State()
     editing_point_name = State()
     editing_point_description = State()
     editing_point_location = State()
+    waiting_for_point_text = State()
+    waiting_for_point_photo = State()
+    waiting_for_point_audio = State()
 
     # Состояния для редактирования маршрута
     editing_route = State() 
