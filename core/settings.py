85617ee872302016962e8241f1d899c08ce9fc34
Дмитрий Захаров <zds.vip1221@gmail.com>
2025-06-23 23:47:10 +0300

    corse headers setting

--- core/settings.py
+++ core/settings.py
@@ -122,5 +122,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    "https://localhost:3000",
+    "http://localhost:3000",
 ]
