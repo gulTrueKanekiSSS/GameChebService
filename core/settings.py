99d61ac9719d58149c87b97551e815cd56577b9f
Дмитрий Захаров <zds.vip1221@gmail.com>
2025-06-26 20:11:15 +0300

    ssl https

--- core/settings.py
+++ core/settings.py
@@ -131,3 +131,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ],
 }
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_HSTS_SECONDS = 60  # или больше, когда убедитесь, что всё работает
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
