d4bd0d683e066f347a342ab3268b068b09021e72
Дмитрий Захаров <zds.vip1221@gmail.com>
2025-07-03 14:35:04 +0300

    permission https://gamecheb.netlify.app/

--- core/settings.py
+++ core/settings.py
@@ -124,7 +124,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://gamecheb.netlify.app",
+    "http://gamecheb.netlify.app",
 ]
 
 REST_FRAMEWORK = {
