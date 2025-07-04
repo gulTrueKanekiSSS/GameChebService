af101e6febea771dd572e9b9c7cd6dcc6abe1559
Дмитрий Захаров <zds.vip1221@gmail.com>
2025-07-04 13:20:20 +0300

    acces for gosha frontend

--- core/settings.py
+++ core/settings.py
@@ -124,7 +124,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://gamecheb.netlify.app",
+    "https://gamecheb.netlify.app",
 ]
 
 REST_FRAMEWORK = {
