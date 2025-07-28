8ba39cc56f953f85f2373da75809d944e612c9dc
Дмитрий Захаров <zds.vip1221@gmail.com>
2025-07-28 11:38:51 +0300

    acces to lera

--- core/settings.py
+++ core/settings.py
@@ -16,6 +16,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'your-secret-key-here')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+
 ALLOWED_HOSTS = [
   'localhost',
   '127.0.0.1',
